3f85a73a9d523526fa22ef4dc19c6443055b6cd1
Scott Baker <smbaker@gmail.com>
2016-07-13 08:32:07 -0700

    add -y to apt commands

--- setup-i2c.sh
+++ setup-i2c.sh
@@ -3,8 +3,8 @@ echo "i2c-bcm2708" >> /etc/modules
 echo "i2c-dev" >> /etc/modules
 reboot
 
-sudo apt-get install python-smbus
-sudo apt-get install i2c-tools
+sudo apt-get -y install python-smbus
+sudo apt-get -y install i2c-tools
 
 sudo emacs /etc/modprobe.d/raspi-blacklist.conf
 # comment out spi-bcm2708 and i2c-bcm2708
