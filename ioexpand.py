f124b4299b54ab276f1cbbe2199ce550c700d35d
Scott Baker <smbaker@gmail.com>
2016-07-13 08:31:31 -0700

    add or_ and not_ gpio functions

--- ioexpand.py
+++ ioexpand.py
@@ -19,6 +19,8 @@ class MCP23017:
        self.addr = addr
        self.bus = bus
        self.banks = 2
+       self.gpio_bits = [0, 0]
+       self.dir_bits = [0, 0]
 
    def writereg(self, reg, bits):
        self.bus.write_byte_data(self.addr, reg, bits)
@@ -28,6 +30,7 @@ class MCP23017:
 
    def set_iodir(self, bank, bits):
        self.writereg(bank + IODIR, bits)
+       self.dir_bits = bits
 
    def set_polarity(self, bank, bits):
        self.writereg(bank + IPOL, bits)
@@ -43,6 +46,7 @@ class MCP23017:
 
    def set_gpio(self, bank, bits):
        self.writereg(bank + MCP23017_GPIO, bits)
+       self.gpio_bits[bank] = bits
 
    def set_latch(self, bank, bits):
        self.writereg(bank + OLAT, bits)
@@ -64,17 +68,34 @@ class MCP23017:
        self.set_pullup(0, 0xFF)
        self.set_iodir(1, 0x00)
 
+   def or_gpio(self, bank, bits):
+        self.set_gpio(bank, self.gpio_bits[bank] | bits)
+
+   def not_gpio(self, bank, bits):
+        bits = ((~bits) & 0xFF)
+        self.set_gpio(bank, self.gpio_bits[bank] & bits)
+
 class PCF8574:
     def __init__(self, bus, addr):
         self.addr = addr
         self.bus = bus
         self.banks = 1
+        self.gpio_bits = 0xFF
 
     def get_gpio(self, bank):
         return self.bus.read_byte(self.addr)
 
     def set_gpio(self, bank, value):
-        return self.bus.write_byte(self.addr, value)
+        self.bus.write_byte(self.addr, value)
+        self.gpio_bits = value
 
     def configure_as_keypad(self):
         self.bus.write_byte(self.addr,0xFF)
+
+    def or_gpio(self, bank, bits):
+        self.set_gpio(bank, self.gpio_bits | bits)
+
+    def not_gpio(self, bank, bits):
+        bits = ((~bits) & 0xFF)
+        self.set_gpio(bank, self.gpio_bits & bits)
+
